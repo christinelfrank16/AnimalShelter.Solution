e26982be9aaff792be81ccfdf1d4af3af782a659
Christine Frank and Kira Roberts <kira@>
2019-10-15 13:32:25 -0700

    Add edit and delete functions to animal mvc

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
@@ -34,6 +35,35 @@ namespace AnimalShelter.Controllers
         {
             Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
             return View(thisAnimal);
+        }
+
+        public ActionResult Edit (int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Animal animal)
+        {
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+            _db.Animals.Remove(thisAnimal);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
